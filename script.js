4448fb3402ff925826986e09ca2a1a992d20624b
Aman Tripathi <amanttripathi02@gmail.com>
2025-12-29 07:24:42 +0530

    created an update message for script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
-const resultDiv = document.getElementById("result");
+const resultMessage = document.getElementById("result");
 const playAgainBtn = document.getElementById("play-again");
 const humanScoreSpanEl = document.getElementById("human-score");
 const computerScoreSpanEl = document.getElementById("computer-score");
 const roundEl = document.getElementById("round");
+const choicesDisplayEl = document.getElementById("choices-display");
 
 const choiceButton = [rockBtn, paperBtn, scissorsBtn];
 
@@ -35,7 +36,7 @@ function determineWinner(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         return "It's a tie!";
     }
-}
+
 
 const winConditions = {
     rock: "scissors",
@@ -43,9 +44,32 @@ const winConditions = {
     scissors: "paper"
 };
 
-if (winConditions[humanChoice] === computerChoice) {
-    return 'win';
-} else {
-    return 'lose';
+    if (winConditions[humanChoice] === computerChoice) {
+        return 'win';
+    } else {
+        return 'lose';
+
+    }
 
 }
+
+
+function updateResultMessage(humanChoice, computerChoice, outcome) {
+    resultMessage.classList.remove("win", "lose", "tie");
+    choicesDisplayEl.textContent = `You chose ${getEmoji(humanChoice)}. Computer chose ${getEmoji(computerChoice)}.`;
+
+    if(outcome === 'win') {
+        resultMessage.textContent = "You win!";
+        resultMessage.classList.add("win");
+    
+    }
+    else if(outcome === 'lose') {
+        resultMessage.textContent = "You lose!";
+        resultMessage.classList.add("lose");
+    }
+    else {
+        resultMessage.textContent = "It's a tie!";
+        resultMessage.classList.add("tie");
+    }
+}
+
